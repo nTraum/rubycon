72212f5c62bbcfa3fb0d67c66db9b87d58ecfba2
nTraum <philipp.press@googlemail.com>
2013-08-08 20:25:04 +0200

    better version testing

--- test/rubycon/version_test.rb
+++ test/rubycon/version_test.rb
@@ -1,6 +1,10 @@
 require_relative File.join(['..','test_helper'])
 
 describe Rubycon do
+  it 'must be defined' do
+    Rubycon::VERSION.wont_be_nil
+  end
+
   it 'must have the right version' do
     Rubycon::VERSION.must_equal '0.3'
   end
